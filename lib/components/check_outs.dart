d8d7d4498cee84304751f5da77237a3c07ce9d4b
Ellis Ifoegbu <niceju4u@gmail.com>
2023-03-08 09:11:42 +0100

    Added splash screen, launcher icon, and renamed app to 'Max Fleet'

--- lib/components/check_outs.dart
+++ lib/components/check_outs.dart
@@ -15,29 +15,6 @@ class _CheckOutsState extends State<CheckOuts> {
   final _listAnimateKey = GlobalKey<SliverAnimatedListState>();
 
   @override
-  void initState() {
-    super.initState();
-
-    final model = context.read<DashboardViewModel>();
-
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      Future future = Future(() {});
-      for (int index = 0; index < model.checkedOutVehicles.length; index++) {
-        future = future.then((_) {
-          return Future.delayed(const Duration(milliseconds: 100), () {
-            _listAnimateKey.currentState!.insertItem(
-              index,
-              duration: const Duration(
-                milliseconds: 250,
-              ),
-            );
-          });
-        });
-      }
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Consumer<DashboardViewModel>(
       builder: (_, model, __) {
@@ -61,28 +38,20 @@ class _CheckOutsState extends State<CheckOuts> {
           initialItemCount: model.checkedOutVehicles.length,
           itemBuilder: (context, index, animation) {
             final vehicle = model.checkedOutVehicles[index];
-            return SlideTransition(
-              position: animation.drive(
-                Tween<Offset>(
-                  begin: const Offset(1, 0),
-                  end: const Offset(0, 0),
-                ),
-              ),
-              child: CheckOutCards(
-                vehicle: model.checkedOutVehicles[index],
-                onAddComplete: () {
-                  _listAnimateKey.currentState!.removeItem(
-                    index,
-                    (context, animation) => SizeTransition(
-                      key: ValueKey(vehicle.referenceNumber),
-                      sizeFactor: animation,
-                      child: CheckOutCards(
-                        vehicle: vehicle,
-                      ),
+            return CheckOutCards(
+              vehicle: model.checkedOutVehicles[index],
+              onAddComplete: () {
+                _listAnimateKey.currentState!.removeItem(
+                  index,
+                  (context, animation) => SizeTransition(
+                    key: ValueKey(vehicle.referenceNumber),
+                    sizeFactor: animation,
+                    child: CheckOutCards(
+                      vehicle: vehicle,
                     ),
-                  );
-                },
-              ),
+                  ),
+                );
+              },
             );
           },
         );
